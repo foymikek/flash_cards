f8971677e594901b391c7fcf1dd2e73311bdaadc
Michael Foy <foymikek@gmail.com>
2020-10-11 09:07:19 -0600

    Add passing test

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -7,12 +7,21 @@ require './lib/deck'
 class DeckTest < Minitest::Test
 
   def test_deck_has_multiple_cards
-     card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-     card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-     cards = [card_1, card_2, card_3]
-     deck = Deck.new(cards)
-     assert_equal cards, deck.cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_equal cards, deck.cards
+  end
+
+  def test_deck_can_count_number_of_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert_equal 3, deck.count
   end
 
 end
