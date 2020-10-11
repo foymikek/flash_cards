ba19ef64fee89a7279bf50d462a8fa84ada8d60f
Michael Foy <foymikek@gmail.com>
2020-10-11 09:57:41 -0600

    Add card_generator class

--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -3,8 +3,10 @@ require 'minitest/pride'
 require './lib/card_generator'
 
 class CardGeneratorTest < Minitest::Test
+
   def test_it_has_attributes
     filename = "./lib/cards.txt"
     cards = CardGenerator.new(filename).cards
   end
+  
 end
